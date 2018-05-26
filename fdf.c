41cd4de18317c84752cace9c37a6889e1dac1af3
Charlotte BAILLE <annecharlottebaille@gmail.com>
2018-05-26 17:31:04 +0200

    test

--- fdf.c
+++ fdf.c
@@ -109,47 +109,46 @@ t_map *ft_open(char *av, t_map *map)
     return (map);
 }
 
-static void		init_line(int *x, int *y, t_point a, t_point b)
-{
-	x[0] = abs(b.x - a.x);
-	y[0] = abs(b.y - a.y);
-	x[1] = (b.x > a.x ? 1 : -1);
-	y[1] = (b.y > a.y ? 1 : -1);
-	x[2] = a.x;
-	y[2] = a.y;
-}
+// static void		init_line(int *x, int *y, t_point a, t_point b)
+// {
+// 	x[0] = abs(b.x - a.x);
+// 	y[0] = abs(b.y - a.y);
+// 	x[1] = (b.x > a.x ? 1 : -1);
+// 	y[1] = (b.y > a.y ? 1 : -1);
+// 	x[2] = a.x;
+// 	y[2] = a.y;
+// }
 
-void			draw_line(t_mlx mlx, t_map *map, t_point a, t_point b)
-{
-	int x[3];
-	int y[3];
-	int err[2];
+// void			draw_line(t_mlx mlx, t_map *map, t_point a, t_point b)
+// {
+// 	int x[3];
+// 	int y[3];
+// 	int err[2];
 
-	init_line(x, y, a, b);
-	err[0] = x[0] - y[0];
-	while (!((b.x == x[2]) && (b.y == y[2])))
-	{
-		// ppixel(env, x[2], y[2], gradient(env, vectorize(&a, &b), x[2], y[2]));
-        mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, ((map[i].point[j].x * 0.8) * 20 + v + 200) + i * 20, (map[i].point[j].y * 0.8 * 20 + v + 200) - j * 20, 0xc71515);
-		err[1] = err[0];
-		if (err[1] > -x[0])
-		{
-			err[0] -= y[0];
-			x[2] += x[1];
-		}
-		if (err[1] <= y[0])
-		{
-			err[0] += x[0];
-			y[2] += y[1];
-		}
-	}
-}
+// 	init_line(x, y, a, b);
+// 	err[0] = x[0] - y[0];
+// 	while (!((b.x == x[2]) && (b.y == y[2])))
+// 	{
+// 		// ppixel(env, x[2], y[2], gradient(env, vectorize(&a, &b), x[2], y[2]));
+// 		err[1] = err[0];
+// 		if (err[1] > -x[0])
+// 		{
+// 			err[0] -= y[0];
+// 			x[2] += x[1];
+// 		}
+// 		if (err[1] <= y[0])
+// 		{
+// 			err[0] += x[0];
+// 			y[2] += y[1];
+// 		}
+// 	}
+// }
 
 void fdf(t_mlx mlx, t_map *map)
 {
     
-    int dx;
-    int dy;
+    // int dx;
+    // int dy;
     // int cumul;
     unsigned int i;
     unsigned int j;
@@ -158,8 +157,9 @@ void fdf(t_mlx mlx, t_map *map)
     while (i < map[0].nblignes)
     {
         j = 0;
-        dx = map[i].point[map[0].nblignes - 1].x - map[i].point[j].x;
-        dy = map[i].point[map[0].nblignes - 1].y - map[i].point[j].y;
+        // dx = map[i].point[map[0].nblignes - 1].x - map[i].point[j].x;
+        // dy = map[i].point[map[0].nblignes - 1].y - map[i].point[j].y;
+        // mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, ((map[i].point[j].x * 0.8) * 20 + v + 200) + i * 20, (map[i].point[j].y * 0.8 * 20 + v + 200) - j * 20, 0xc71515);
         // if (dx != 0)
         //     if (dx > 0)
         //     {
@@ -176,9 +176,9 @@ void fdf(t_mlx mlx, t_map *map)
             if (j != map[0].nbpoints - 1)
                 for (int v = 0; v < 20; v++)
                     mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, ((map[i].point[j].x * 0.8) * 20 + v + 200) + i * 20, (map[i].point[j].y * 0.8 * 20 + v + 200) - j * 20, 0xc71515);
-            // if (i != map[0].nblignes - 1)
-            //     for (unsigned int z = 0; z < 20; z++)
-            //         mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, ((map[i].point[j].x * 0.8) * 20 + 200) + (i * 20), (map[i].point[j].y * 0.8 * 20 + z + 200) - (j * 20), 0xc71515);     
+            if (i != map[0].nblignes - 1)
+                for (unsigned int z = 0; z < 20; z++)
+                    mlx_pixel_put(mlx.mlx_ptr, mlx.win_ptr, ((map[i].point[j].x * 0.8) * 20 + 200) + (i * 20), (map[i].point[j].y * 0.8 * 20 + z + 200) - (j * 20), 0xc71515);     
             j++;
         }
         i++;
